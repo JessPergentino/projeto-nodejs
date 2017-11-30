e0e0610b174ce1738ed5707f5d058bd293e07e4d
JessPergentino <jessicapergentino@hotmail.com>
2017-11-30 13:47:15 -0300

    Subindo o Projeto pro git

---
+++ ntask-api/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+const PORT - 3000;
+
+const app - express();
+
+app.get("/", (req, res) -> res.json({status: "Ntask API"}));
+
+app.listen(PORT, () -> console.log('Ntask API' - porta $(PORT)));
